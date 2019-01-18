aa514b357c98a129f14929d14abf2091e5180f0d
Mateus Danielle <mateusdanielle@edu.fecap.br>
2019-01-18 12:46:19 -0200

    Fila com Redis

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`)
+    await Mail.send(
+      ['emails.new_task'],
+      { username, title, hasAttachment: !!file },
+      message => {
+        message
+          .to(email)
+          .from('mateusdanielle@edu.fecap.br', 'Mateus | Danielle')
+          .subject('Nova tarefa para você')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
